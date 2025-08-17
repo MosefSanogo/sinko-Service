b85b54c6ce6b0da78c4b65b7232a34d1d706da86
MosefSanogo <mosefsanogo366@gmail.com>
2025-08-17 16:38:32 +0200

    Mises à jour du code, Changement de l'url des images et ajout de basename

--- src/pages/immobilier/Immobilier.jsx
+++ src/pages/immobilier/Immobilier.jsx
@@ -5,19 +5,18 @@ function Immobilier() {
         <div className='immobilier'>
             <img src={`${import.meta.env.BASE_URL}/immeuble.png`} alt="Immobilier" className='immobilier__image' />
             <h1 className='immobilier__title'>Immobilier</h1>
-            <p className='immobilier__description'>
-                SINKO SERVICES vous accompagne dans toutes vos transactions immobilières :<br /><br />
+            <div className='immobilier__description'>
+                <p>SINKO SERVICES vous accompagne dans toutes vos transactions immobilières :</p>
 
-                . Vente et achat de terrains, maisons et appartements.<br />
+                <ul>
+                    <li>Vente et achat de terrains, maisons et appartements.</li>
+                    <li>Location de biens résidentiels et commerciaux.</li>
+                    <li>Gestion et valorisation de votre patrimoine immobilier.</li>
+                    <li>Conseil pour vos projets d’investissement.</li>
+                </ul>
 
-                . Location de biens résidentiels et commerciaux.<br />
-
-                . Gestion et valorisation de votre patrimoine immobilier.<br />
-
-                . Conseil pour vos projets d’investissement.<br /><br />
-
-                Notre engagement : un service fiable, rapide et transparent, pour vous offrir des solutions sur mesure dans les domaines du nettoyage et de l’immobilier.
-            </p>
+                <p>Notre engagement : un service fiable, rapide et transparent, pour vous offrir des solutions sur mesure dans les domaines du nettoyage et de l’immobilier.</p>
+            </div>
         </div>
     )
 }
